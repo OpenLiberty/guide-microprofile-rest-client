bbe9ba4bc06ec1bfefde3896d2aad5ca69cfbbad
Ahmad Ayyoub <Ahmad.Ayyoub@ibm.com>
2018-04-05 16:10:07 -0400

    Testing recursive push

--- script.sh
+++ script.sh
@@ -1,55 +1,43 @@
 #!/bin/bash
 
-# check for missing files: start , finish, REAMDE.adoc.
-
-# echo -n -e "\033[1;34mPlease enter repository name: \033[0m"
-# read -e repoName
-# if [ ! -d "./$repoName" ]
-# then
-#    echo -e "\033[1;31mError: directory ./$repoName does not exists.\033[0m"
-#    exit
-# fi
-
-# cd $repoName
-
-echo -e "\033[1;32m\nChecking the differences in start and finish directories...\n\033[0m"
+echo -e "\033[1;32mChecking the differences in start and finish directories...\033[0m"
 diff -r start finish
 
 ##################################################################
 
-echo -e "\033[1;32m\nRemoving cached files in .git directory\n\033[0m"
+# echo -e "\033[1;32m\nRemoving cached files in .git directory\n\033[0m"
 
-declare -a files=("zip" "jar" "DS_Store" "target")
+# declare -a files=("zip" "jar" "DS_Store" "target")
 
-for i in "${files[@]}"
-do
-  if [ "$i" == "target" ]
-  then
-    git filter-branch --index-filter "git rm --cached --ignore-unmatch -r $i" -- --all
-  else
-    git filter-branch --index-filter "git rm --cached --ignore-unmatch *.$i" -- --all
-  fi
-  rm -Rf .git/refs/original
-  rm -Rf .git/logs/
-  git gc --aggressive --prune=now
-done
+# for i in "${files[@]}"
+# do
+#   if [ "$i" == "target" ]
+#   then
+#     git filter-branch --index-filter "git rm --cached --ignore-unmatch -r $i" -- --all
+#   else
+#     git filter-branch --index-filter "git rm --cached --ignore-unmatch *.$i" -- --all
+#   fi
+#   rm -Rf .git/refs/original
+#   rm -Rf .git/logs/
+#   git gc --aggressive --prune=now
+# done
 
-output=$(du -hks)
-size=$(echo $output | cut -c1-3)
+# output=$(du -hks)
+# size=$(echo $output | cut -c1-3)
 
-if [ $size -gt 1000 ]
-then
-   echo -e "\033[1;31m\nYour repository is large.\n\033[0m"
-   exit
-fi
+# if [ $size -gt 1000 ]
+# then
+#    echo -e "\033[1;31m\nYour repository is large.\n\033[0m"
+#    exit
+# fi
 
-echo -n -e "\033[1;34mDo you want to push(Y/N)?  \033[0m"
-read -e answer
-if [ "$answer" == "Y" ] || [ "$answer" == "y" ] ; then
- echo -e "Pushed...\n"
-else
- echo -e ""
-fi
+# echo -n -e "\033[1;34mDo you want to push(Y/N)?  \033[0m"
+# read -e answer
+# if [ "$answer" == "Y" ] || [ "$answer" == "y" ] ; then
+#  echo -e "Pushed...\n"
+# else
+#  echo -e ""
+# fi
 
 #############################################################
 
@@ -76,7 +64,7 @@ fi
 
 sed -i -e 's/`src/`\/src/g' README.adoc
 git add *
-git commit -m "Travis-Ci Pushed"
+git commit -m "Travis-Ci Pushed..."
 git push 
 
 actualPaths=($(find "finish" -type f | cut -c8-))
