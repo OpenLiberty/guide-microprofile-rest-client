5b36fbb78cecbeb9a5b1729e920b86a528f1bd83
Ahmad Ayyoub <Ahmad.Ayyoub@ibm.com>
2018-04-05 15:48:33 -0400

    Modified adoc

--- script.sh
+++ script.sh
@@ -17,111 +17,84 @@ diff -r start finish
 
 ##################################################################
 
-# echo -e "\033[1;32m\nRemoving cached files in .git directory\n\033[0m"
+echo -e "\033[1;32m\nRemoving cached files in .git directory\n\033[0m"
 
-# declare -a files=("zip" "jar" "DS_Store" "target")
+declare -a files=("zip" "jar" "DS_Store" "target")
 
-# for i in "${files[@]}"
-# do
-#   if [ "$i" == "target" ]
-#   then
-#     git filter-branch --index-filter "git rm --cached --ignore-unmatch -r $i" -- --all
-#   else
-#     git filter-branch --index-filter "git rm --cached --ignore-unmatch *.$i" -- --all
-#   fi
-#   rm -Rf .git/refs/original
-#   rm -Rf .git/logs/
-#   git gc --aggressive --prune=now
-# done
+for i in "${files[@]}"
+do
+  if [ "$i" == "target" ]
+  then
+    git filter-branch --index-filter "git rm --cached --ignore-unmatch -r $i" -- --all
+  else
+    git filter-branch --index-filter "git rm --cached --ignore-unmatch *.$i" -- --all
+  fi
+  rm -Rf .git/refs/original
+  rm -Rf .git/logs/
+  git gc --aggressive --prune=now
+done
 
-# output=$(du -hks)
-# size=$(echo $output | cut -c1-3)
+output=$(du -hks)
+size=$(echo $output | cut -c1-3)
 
-# if [ $size -gt 1000 ]
-# then
-#    echo -e "\033[1;31m\nYour repository is large.\n\033[0m"
-#    exit
-# fi
+if [ $size -gt 1000 ]
+then
+   echo -e "\033[1;31m\nYour repository is large.\n\033[0m"
+   exit
+fi
 
-# echo -n -e "\033[1;34mDo you want to push(Y/N)?  \033[0m"
-# read -e answer
-# if [ "$answer" == "Y" ] || [ "$answer" == "y" ] ; then
-#  echo -e "Pushed...\n"
-# else
-#  echo -e ""
-# fi
+echo -n -e "\033[1;34mDo you want to push(Y/N)?  \033[0m"
+read -e answer
+if [ "$answer" == "Y" ] || [ "$answer" == "y" ] ; then
+ echo -e "Pushed...\n"
+else
+ echo -e ""
+fi
 
-##############################################################
+#############################################################
 
-echo -e "\033[1;32mChecking the files to be commited:\n\033[0m"
+declare -a words=('we ' 'you will' 'lets' 'let us' "let\'s" 'has been' 'have been' 'was')
+clean=true
+for word in "${words[@]}"
+do
+	wordCount=$(grep -o -i "$word" README.adoc | wc -l | sed -e 's/^[ \t]*//')
+	linesNumber=$(grep -i -n "$word" README.adoc | cut -f1 -d:)
+	if [ "$wordCount" -ge 1 ]
+	then
+	echo -e "You have $wordCount words of \033[1;31m'$word'\033[0m in README.adoc file at following lines:"
+	echo -e "$linesNumber\n"
+	clean=false
+	fi
+done
 
-committedFiles=$(git diff --stat --cached origin/master)
+if [ clean == true ]
+then
+echo "README.adoc is clean"
+fi
 
-echo -e "$committedFiles\n"
+#################################################################
 
-##############################################################
+sed -i -e 's/`src/`\/src/g' README.adoc
+git add *
+git commit -m "Travis-Ci Pushed"
+git push 
 
-# For structure: https://unix.stackexchange.com/questions/266757/go-through-every-subfolder-check-for-a-folder-and-run-script/266800
-# https://github.com/goodmami/check.bash
-# find A -type d -name target -execdir foo.sh \;
+actualPaths=($(find "finish" -type f | cut -c8-))
+writtenPaths=($(grep -o '`src.*`' README.adoc | sed -e 's/^`//' -e 's/`$//'))
+for writtenPath in "${writtenPaths[@]}"
+do
+	for actualPath in "${actualPaths[@]}"
+	do
+		writtenFile=$(echo "$writtenPath" | rev | cut -d'/' -f1 | rev)
+		actualFile=$(echo "$actualPath" | rev | cut -d'/' -f1 | rev)
+		if  [ "$writtenFile" == "$actualFile" ]
+		then
+			if [ "$writtenPath" != "$actualPath" ]
+			then
+			echo -e "\033[1;31mWARNING:\033[0m $writtenPath \033[1;31m<-->\033[0m $actualPath"
+			fi
+		fi
+	done
+done
 
-# https://stackoverflow.com/questions/29967444/read-multiple-inputs-and-store-in-a-array-then-print-the-values-from-that-array
-
-# done=false
-# startFiles=()
-# echo
-# for ((i=1;; i++)); do
-# 	read -p $'\033[1;34mPlease enter the file(s) that should not be in start directory: \033[0m' startFiles[$i]
-# 	break
-# done
-
-
-# for file in "${startFiles[@]}"
-# do
-# 	find "./start" -name "$file" | echo -e "\033[1;31mError: $file not found\033[0m"
-# done
-
-# declare -a words=('we ' 'you will' 'lets' 'let us' "let\'s" 'has been' 'have been' 'was')
-# clean=true
-# for word in "${words[@]}"
-# do
-# 	wordCount=$(grep -o -i "$word" README.adoc | wc -l | sed -e 's/^[ \t]*//')
-# 	linesNumber=$(grep -i -n "$word" README.adoc | cut -f1 -d:)
-# 	if [ "$wordCount" -ge 1 ]
-# 	then
-# 	echo -e "You have $wordCount words of \033[1;31m'$word'\033[0m in README.adoc file at following lines:"
-# 	echo -e "$linesNumber\n"
-# 	clean=false
-# 	fi
-# done
-
-# if [ clean == true ]
-# then
-# echo "README.adoc is clean"
-# fi
-
-# #################################################################
-
-# ### change `/src -> `src
-# sed -i -e 's/`\/src/`src/g' README.adoc
-# echo "Changed /src -> src"
-
-# actualPaths=($(find "finish" -type f | cut -c8-))
-# writtenPaths=($(grep -o '`src.*`' README.adoc | sed -e 's/^`//' -e 's/`$//'))
-# for writtenPath in "${writtenPaths[@]}"
-# do
-# 	for actualPath in "${actualPaths[@]}"
-# 	do
-# 		writtenFile=$(echo "$writtenPath" | rev | cut -d'/' -f1 | rev)
-# 		actualFile=$(echo "$actualPath" | rev | cut -d'/' -f1 | rev)
-# 		if  [ "$writtenFile" == "$actualFile" ]
-# 		then
-# 			if [ "$writtenPath" != "$actualPath" ]
-# 			then
-# 			echo -e "\033[1;31mWARNING:\033[0m $writtenPath \033[1;31m<-->\033[0m $actualPath"
-# 			fi
-# 		fi
-# 	done
-# done
-
-# grep -i 'create.*in.*the.*`src.*`.*file.*:' README.adoc
+grep -i 'create.*in.*the.*`src.*`.*file.*:' README.adoc
