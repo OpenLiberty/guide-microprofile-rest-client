df9d3f164eb0ff95374ace2cff388ac03b3181f1
Ahmad Ayyoub <ahmadosamaayyoub@gmail.com>
2018-04-16 10:36:01 -0400

    test

--- script.sh
+++ script.sh
@@ -4,10 +4,18 @@ cd finish
 mvn clean
 mavenOutput=$(mvn liberty:install-server)
 build=$(echo "$mavenOutput" | grep -i "runtime/" | cut -d'-' -f 2 | cut -d'/' -f 2 )
-IFS='\n' read -r -a array <<< "$build"
+IFS=' ' read -r -a array <<< "$build"
 cd ..
-echo "\033[1;34mOpenLiberty runtime:\033[0m ${array[5]}\n"
-echo "\033[1;34mOpenLiberty runtime:\033[0m $build\n"
+echo "\033[1;34mOpenLiberty runtime:\033[0m ${array[0]}"
+echo "\033[1;34mOpenLiberty runtime:\033[0m ${array[1]}"
+echo "\033[1;34mOpenLiberty runtime:\033[0m ${array[2]}"
+echo "\033[1;34mOpenLiberty runtime:\033[0m ${array[3]}"
+echo "\033[1;34mOpenLiberty runtime:\033[0m ${array[4]}"
+echo "\033[1;34mOpenLiberty runtime:\033[0m ${array[5]}"
+echo "\033[1;34mOpenLiberty runtime:\033[0m ${array[6]}"
+echo "\033[1;34mOpenLiberty runtime:\033[0m ${array[7]}"
+echo "\033[1;34mOpenLiberty runtime:\033[0m ${array[8]}"
+echo "\033[1;34mOpenLiberty runtime:\033[0m $build"
 #############################################################################################
 echo -e "\033[1;32m[1] Checking the differences in start and finish folders ...\033[0m"
 status_1=0
