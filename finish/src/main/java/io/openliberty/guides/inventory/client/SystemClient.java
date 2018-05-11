5f251216136f210110b7c2c876e983d4d684d2ec
Junyi Sun <junyisun@junyis-mbp.war.can.ibm.com>
2018-05-11 17:12:45 -0400

    update license

--- finish/src/main/java/io/openliberty/guides/inventory/client/SystemClient.java
+++ finish/src/main/java/io/openliberty/guides/inventory/client/SystemClient.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2017, 2018 IBM Corporation and others.
+ * Copyright (c) 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -30,7 +30,7 @@ import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 @Path("/properties")
 public interface SystemClient {
   // end::annotations[]
-  
+
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   public Properties getProperties() throws UnknownUrlException, ProcessingException;
--- finish/src/main/java/io/openliberty/guides/inventory/client/UnknownUrlExceptionMapper.java
+++ finish/src/main/java/io/openliberty/guides/inventory/client/UnknownUrlExceptionMapper.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2017, 2018 IBM Corporation and others.
+ * Copyright (c) 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -27,7 +27,7 @@ public class UnknownUrlExceptionMapper
   @Override
   public boolean handles(int status, MultivaluedMap<String, Object> headers) {
     LOG.info("status = " + status);
-    return status == 404; 
+    return status == 404;
   }
 
   @Override
