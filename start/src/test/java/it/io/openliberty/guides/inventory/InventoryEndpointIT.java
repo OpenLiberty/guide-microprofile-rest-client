1c9f7fa8e618a537492be3b1d6455a7a8b427a74
Tiago Ferreira <t.ferreiraibm.com@Tiagos-MacBook-Pro.local>
2020-05-28 11:12:55 -0400

    update to new reqs

--- start/src/test/java/it/io/openliberty/guides/inventory/InventoryEndpointIT.java
+++ start/src/test/java/it/io/openliberty/guides/inventory/InventoryEndpointIT.java
@@ -15,7 +15,6 @@ package it.io.openliberty.guides.inventory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import javax.json.JsonArray;
 import javax.json.JsonObject;
@@ -137,12 +136,8 @@ public class InventoryEndpointIT {
     "BadResponse expected status: 404. Response code not as expected.");
 
     String stringObj = badResponse.readEntity(String.class);
-    assertEquals("{}", stringObj, "Response entity is not as expected.");
-
-    try {
-        JsonObject jsonObj = badResponse.readEntity(JsonObject.class);
-        fail("Object should not be able to be read as JSON object");
-    } catch (Exception e) {}
+    assertTrue(stringObj.contains("ERROR"), 
+    "badhostname is not a valid host but it didn't raise an error");
 
     response.close();
     badResponse.close();
